ae81ccd8ae0ff350a285c79c5abba656661e45ac
Quinn Tucker <qxzcraft@gmail.com>
2025-12-28 22:00:34 +0000

    Fix deprecation warning

--- packages/hydra/src/hydra/modules/ops.py
+++ packages/hydra/src/hydra/modules/ops.py
@@ -51,7 +51,7 @@ def ssm_params(xBC, D_weight, D_bias, d_inner, headdim, ngroups):
 
 class HydraSplitConv1dScanCombinedFn(torch.autograd.Function):
     @staticmethod
-    @torch.cuda.amp.custom_fwd
+    @torch.amp.custom_fwd(device_type="cuda")
     def forward(
         ctx,
         zxBCdt,
@@ -195,7 +195,7 @@ class HydraSplitConv1dScanCombinedFn(torch.autograd.Function):
         return out
 
     @staticmethod
-    @torch.cuda.amp.custom_bwd
+    @torch.amp.custom_bwd(device_type="cuda")
     def backward(ctx, dout, *args):
         (
             z,
