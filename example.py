d68fe5ed3236152ebce99a2e5f8d068a452b5b75
Ben Giacalone <bensgiacalone@gmail.com>
2025-12-29 20:20:46 +0000

    Update `example.py` with C4 dataset

--- example.py
+++ example.py
@@ -1,10 +1,9 @@
-"""
-Example script taken from hnet-impl's original repo.
-"""
-
 import os
+from collections.abc import Generator
+from typing import Any
 
 import torch
+from datasets import load_dataset
 from torch import Tensor as TT
 from torch import nested
 from torch.distributed import device_mesh as tdm
@@ -12,79 +11,86 @@ from torch.distributed import fsdp
 
 from hnet_impl import ByteTokenizer, HNetConfig, HNetLM, completion_sync
 
-## dist init
-world_size = int(os.environ.get("WORLD_SIZE", "1"))
-local_rank = int(os.environ.get("LOCAL_RANK", "0"))
-torch.cuda.set_device(local_rank)
-if world_size > 1:
-    mesh = tdm.init_device_mesh("cuda", (world_size,), mesh_dim_names=("dp",))
 
-## create model
-t = ByteTokenizer()
-c = HNetConfig.create_reasonable_config(D=[512, 1024], arch=["m4", "T9"])
-with torch.device("cuda"):
-    m = HNetLM(c)
+def get_one_example(ds_iter: Generator, max_seq_len: int) -> str:
+    return next(ds_iter)["text"][:max_seq_len]
 
-## fsdp/compile
-m.backbone.block_compile(ac=False)
-m.apply_fsdp(  # default: BF16, ZeRO2, 1D mesh
-    m,
-    mp_policy=fsdp.MixedPrecisionPolicy(param_dtype=torch.bfloat16),
-    reshard_after_forward=False,
-    mesh=mesh["dp"],
-) if world_size > 1 else m
-
-## optim / lr sched
-base_lr, max_steps = 3e-4, 1000
-opt = torch.optim.AdamW(
-    [dict(params=ls, lr=base_lr * lr_mod) for ls, lr_mod in zip(m.split_params_by_hierachy(), c.lambda_s())],
-    betas=(0.9, 0.95),
-    weight_decay=0.01,
-)
-lrs = torch.optim.lr_scheduler.LambdaLR(
-    opt,
-    lambda step: (pct := step / max_steps) and (pct * 10 if pct < 0.1 else (1 if pct < 0.9 else (1 - pct) * 10)),
-)
-
-
-## example dumb task: random number of repeating letters
-def generate_random_letters():
-    from random import randint
-    from string import ascii_lowercase
-
-    return "".join(randint(0, 10) * c for c in ascii_lowercase)
-
-
-def NJT(ls: list[TT]):
+def NJT(ls: list[TT]) -> TT:
     return nested.nested_tensor(ls, layout=torch.jagged)
 
 
-def random_batches():
+def random_batches(t: ByteTokenizer, ds_iter: Generator, max_seq_len: int) -> Generator[tuple[TT, TT], Any, None]:
     while True:
-        tokens = t.encode([generate_random_letters() for _ in range(32)])
+        tokens = t.encode([get_one_example(ds_iter, max_seq_len) for _ in range(32)])
         iids = [s[:-1] for s in tokens]
         lbls = [s[1:] for s in tokens]
         yield NJT(iids), NJT(lbls).long()
 
+def main() -> None:
+    ## dist init
+    world_size = int(os.environ.get("WORLD_SIZE", "1"))
+    local_rank = int(os.environ.get("LOCAL_RANK", "0"))
+    torch.cuda.set_device(local_rank)
+    mesh = tdm.init_device_mesh("cuda", (world_size,), mesh_dim_names=("dp",))
 
-with m.sampling_mode():
-    print(completion_sync("", t, m, max_new=99))
+    ## create model
+    t = ByteTokenizer()
+    c = HNetConfig.create_reasonable_config(D=[512, 1024], arch=["m4", "T9"])
+    with torch.device("cuda"):
+        m = HNetLM(c)
 
-## training loop
-zero = torch.tensor(0.0, device="cuda")
-for step, (iids, lbls) in zip(range(max_steps), random_batches()):
-    with torch.autocast("cuda", torch.bfloat16):
-        (l_avg, l_sum), extra = m(iids.cuda(), lbls.cuda())
-        l_ratio = sum([e.loss_ratio for e in extra], zero)
-        loss = l_avg + l_ratio
-    loss.backward()
+    ## fsdp/compile
+    m.backbone.block_compile(ac=False)
+    m.apply_fsdp(  # default: BF16, ZeRO2, 1D mesh
+        m,
+        mp_policy=fsdp.MixedPrecisionPolicy(param_dtype=torch.bfloat16),
+        reshard_after_forward=False,
+        mesh=mesh["dp"],
+    ) if world_size > 1 else m
 
-    opt.step()
-    opt.zero_grad()
-    lrs.step()
+    ## optim / lr sched
+    base_lr, max_steps = 3e-4, 1000
+    opt = torch.optim.AdamW(
+        [dict(params=ls, lr=base_lr * lr_mod) for ls, lr_mod in zip(m.split_params_by_hierachy(), c.lambda_s())],
+        betas=(0.9, 0.95),
+        weight_decay=0.01,
+    )
+    lrs = torch.optim.lr_scheduler.LambdaLR(
+        opt,
+        lambda step: (pct := step / max_steps) and (pct * 10 if pct < 0.1 else (1 if pct < 0.9 else (1 - pct) * 10)),
+    )
 
-    if step % 10 == 0:
-        print(f"{step=}: {l_avg.item()=:.3f} {l_ratio.item()=:.3f}")
 
-with m.sampling_mode():
-    print(completion_sync("", t, m, max_new=99))
+    max_seq_len = 512 # 1024 causes OOM
+    log_incr = 10
+    max_new = max_steps // log_incr - 1
+    with m.sampling_mode():
+        print(completion_sync("", t, m, max_new=max_new))
+
+    # Load dataset
+    ds = load_dataset("allenai/c4", "en", streaming=True)
+    ds_train = ds["train"]
+    ds_train = ds_train.shuffle(0)
+    ds_train_iter = iter(ds_train)
+
+    ## training loop
+    zero = torch.tensor(0.0, device="cuda")
+    for step, (iids, lbls) in zip(range(max_steps), random_batches(t, ds_train_iter, max_seq_len)):
+        with torch.autocast("cuda", torch.bfloat16):
+            (l_avg, l_sum), extra = m(iids.cuda(), lbls.cuda())
+            l_ratio = sum([e.loss_ratio for e in extra], zero)
+            loss = l_avg + l_ratio
+        loss.backward()
+
+        opt.step()
+        opt.zero_grad()
+        lrs.step()
+
+        if step % log_incr == 0:
+            print(f"{step=}: {l_avg.item()=:.3f} {l_ratio.item()=:.3f}")
+
+    with m.sampling_mode():
+        print(completion_sync("", t, m, max_new=max_new))
+
+if __name__ == "__main__":
+    main()
